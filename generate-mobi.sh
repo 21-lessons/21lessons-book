04038cebdc1a270e49edc179543ceebe320a6189
Gigi <dergigi@pm.me>
2019-12-27 21:55:16 +0100

    Use tex4ebook config

--- generate-mobi.sh
+++ generate-mobi.sh
@@ -2,4 +2,4 @@
 # Adatped from https://www.lode.de/blog/how-to-create-a-kindle-ebook-with-latex/
 # Requires: tex4ebook, kindlegen
 
-tex4ebook -s -f mobi -e ebook/build.mk4 main.tex
+tex4ebook -c ebook/tex4ebook.cfg -s -f mobi -e ebook/build.mk4 main.tex
